3a950a754d611b9a1ae01c4824e7917c3f9a075f
Smith Grisales <70662445+Yamete-Kudasai@users.noreply.github.com>
2024-02-20 12:26:16 -0500

    Se agregan los campos de nombre, cedula, tipo y se agregan integtracion de iconos dependiendo del grupo poblacional

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const resultado = document.getElementById('resultado');
 const retornoButton = document.getElementById('retorno');
 
 retornoButton.addEventListener('click', () => {
+   
+    formularioCalculadora.reset();
+    
+    resultado.innerHTML = '';
+    
+    
     mostrarFormulario();
 });
 
@@ -12,6 +18,9 @@ formularioCalculadora.addEventListener('submit', (evento) => {
 });
 
 function calcularCalorias() {
+    const nombre = document.querySelector('#nombre').value;
+    const tipoDocumento = document.querySelector('#tipo-documento').value;
+    const documento = document.querySelector('#documento').value;
     const edad = document.querySelector('#edad').value;
     const peso = document.querySelector('#peso').value;
     const altura = document.querySelector('#altura').value;
@@ -24,6 +33,37 @@ function calcularCalorias() {
         edad: 5,
     };
 
+    let mensajeEdad = '';
+    let imagenEdad = '';
+
+
+    if(sexo === 'F'){
+        if(edad >=15 && edad <= 29){
+            mensajeEdad = 'Joven';
+            imagenEdad = 'img/mujer.png';
+        }else if (edad >= 30 && edad <= 59){
+            mensajeEdad = 'Adulto';
+            imagenEdad = 'img/adulta.png';
+        }else{
+            mensajeEdad = 'Adulto Mayor';
+            imagenEdad = 'img/anciana.png';
+        }
+    }else{
+        if(edad >=15 && edad <= 29){
+            mensajeEdad = 'Joven';
+            imagenEdad = 'img/hombre.png';
+        }else if (edad >= 30 && edad <= 59){
+            mensajeEdad = 'Adulto';
+            imagenEdad = 'img/adulto.png';
+        }else{
+            mensajeEdad = 'Adulto Mayor';
+            imagenEdad = 'img/anciano.png';
+        }
+    }
+    
+
+
+
     let calculoCalorias = sexo === 'femenino' ?
     actividad * ((multiplicadorTMB.peso * peso) +
     (multiplicadorTMB.altura * altura) -
@@ -33,9 +73,10 @@ function calcularCalorias() {
     (multiplicadorTMB.edad * edad)) - 5;
 
     resultado.innerHTML = `
-        <h3>Resultado</h3>
-        <input class="form-control text-center" value="${Math.floor(calculoCalorias)} kcal" style="font-size: 2rem; background-color: transparent;" disabled>
-        
+        <h3>${nombre} - ${mensajeEdad}</h3>
+        <img src="${imagenEdad}" alt="${mensajeEdad}" style="width:150px; margin: 30px;">
+        <p class="text-center" style="font-size: 20px;">El paciente ${nombre} identificado con ${tipoDocumento} NO.${documento}, requiere un total de <span class="h6" style="font-size: 20px;>${Math.floor(calculoCalorias)} kcal</span> para el sostenimiento de su TBM.</p>
+
     `
 
     ocultarFormulario();
@@ -48,14 +89,15 @@ function ocultarFormulario() {
     resultado.classList.remove('hidden');
     retornoButton.style.display = '';
     retornoButton.classList.remove('hiden');
-    
+    document.body.classList.add('body-reset')
 }
 function mostrarFormulario() {
     formularioCalculadora.style.display = '';
-    resultado.classList.add('hidden'); 
     formularioCalculadora.classList.remove('hidden');
     retornoButton.classList.add('hiden');
     retornoButton.style.display = 'none';
+    formularioCalculadora.classList.add('mostrar-animacion');
+    document.body.classList.remove('body-reset')
 
 }
  
